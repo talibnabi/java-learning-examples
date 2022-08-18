c0ed4b8352de673aaef70384326ee3748839378c
talibnabi <talibnabiyev@gmail.com>
2022-08-18 04:34:12 -0700

    commit

---
+++ functional-programming/src/main/java/imperative/Main.java
@@ -0,0 +1,46 @@
+package imperative;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Person> people = List.of(
+                new Person("Alisa", Gender.FEMALE),
+                new Person("Alex", Gender.MALE),
+                new Person("Salim", Gender.MALE),
+                new Person("Nurana", Gender.FEMALE)
+        );
+        List<Person> females = new ArrayList<>();
+        for (Person person : people) {
+            if (Gender.FEMALE.equals(person.gender)) {
+                females.add(person);
+            }
+        }
+        for (Person female : females) {
+            System.out.println(female);
+        }
+    }
+
+    private static class Person {
+        private final String name;
+        private final Gender gender;
+
+        public Person(String name, Gender gender) {
+            this.name = name;
+            this.gender = gender;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", gender=" + gender +
+                    '}';
+        }
+    }
+
+    private enum Gender {
+        MALE, FEMALE;
+    }
+}
