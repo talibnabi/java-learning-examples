90bf6e7976f1b1a23e85f4981ca148e4cfa6fc24
talibnabi <talibnabiyev@gmail.com>
2022-08-19 16:48:52 -0700

    commit

--- functional-programming/src/main/java/functional/_Consumer.java
+++ functional-programming/src/main/java/functional/_Consumer.java
@@ -1,5 +1,11 @@
 package functional;
 
+import java.util.function.Consumer;
+
 public class _Consumer {
-    
+    public static void main(String[] args) {
+        customerConsumer.accept(new Customer("Alisa", "99999"));
+    }
+
+    private static final Consumer<Customer> customerConsumer = customer -> System.out.println(customer.toString());
 }
