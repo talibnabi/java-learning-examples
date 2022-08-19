dd5aae31a99ce779bba963003daf94b53ed02cc0
talibnabi <talibnabiyev@gmail.com>
2022-08-19 16:46:01 -0700

    commit

--- functional-programming/src/main/java/functional/Customer.java
+++ functional-programming/src/main/java/functional/Customer.java
@@ -1,4 +1,28 @@
 package functional;
 
+
 public class Customer {
+    private final String customerName;
+    private final String customerPhone;
+
+    public Customer(String customerName, String customerPhone) {
+        this.customerName = customerName;
+        this.customerPhone = customerPhone;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getCustomerPhone() {
+        return customerPhone;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerName='" + customerName + '\'' +
+                ", customerPhone='" + customerPhone + '\'' +
+                '}';
+    }
 }
