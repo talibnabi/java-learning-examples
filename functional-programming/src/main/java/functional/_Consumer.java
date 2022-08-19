acb9f0777183a8a0ea03fab095f023d24a7ad188
talibnabi <talibnabiyev@gmail.com>
2022-08-19 16:59:12 -0700

    commit

--- functional-programming/src/main/java/functional/_Consumer.java
+++ functional-programming/src/main/java/functional/_Consumer.java
@@ -1,11 +1,15 @@
 package functional;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class _Consumer {
     public static void main(String[] args) {
-        customerConsumer.accept(new Customer("Alisa", "99999"));
+//        customerConsumer.accept(new Customer("Alisa", "99999"));
+        customerConsumerV.accept(new Customer("Alisa", "99999"), true);
     }
 
     private static final Consumer<Customer> customerConsumer = customer -> System.out.println(customer.toString());
+    private static final BiConsumer<Customer, Boolean> customerConsumerV =
+            (customer, check) -> System.out.println(customer.getCustomerName() + " " + (check ? customer.getCustomerPhone() : "10"));
 }
