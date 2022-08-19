6af2ef161e37aee549bc5d4a753d9ee85a70938c
talibnabi <talibnabiyev@gmail.com>
2022-08-19 14:45:00 -0700

    commit

--- functional-programming/src/main/java/imperative/Main.java
+++ functional-programming/src/main/java/imperative/Main.java
@@ -1,73 +1,11 @@
 package imperative;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-
-
 public class Main {
-    private static List<Person> people = List.of(
-            new Person("Alisa", Gender.FEMALE),
-            new Person("Alex", Gender.MALE),
-            new Person("Salim", Gender.MALE),
-            new Person("Nurana", Gender.FEMALE)
-    );
+
 
     public static void main(String[] args) {
 
     }
 
-    private static void predicateFunc() {
-        //Declarative approach
-        Predicate<Person> personPredicate = person -> Gender.FEMALE.equals(person.gender);
 
-        List<Person> females2 = people.stream()
-                .filter(personPredicate)
-                .toList();
-        females2.forEach(System.out::println);
-    }
-
-    private static void imperativeApproach() {
-        //Imperative approach
-        List<Person> females = new ArrayList<>();
-        for (Person person : people) {
-            if (Gender.FEMALE.equals(person.gender)) {
-                females.add(person);
-            }
-        }
-        for (Person female : females) {
-            System.out.println(female);
-        }
-    }
-
-    private static void declarativeApproach() {
-        //Declarative approach
-        List<Person> females2 = people.stream()
-                .filter(person -> Gender.FEMALE.equals(person.gender))
-                .toList();
-        females2.forEach(System.out::println);
-    }
-
-    private static class Person {
-        private final String name;
-        private final Gender gender;
-
-        public Person(String name, Gender gender) {
-            this.name = name;
-            this.gender = gender;
-        }
-
-        @Override
-        public String toString() {
-            return "Person{" +
-                    "name='" + name + '\'' +
-                    ", gender=" + gender +
-                    '}';
-        }
-    }
-
-    private enum Gender {
-        MALE, FEMALE;
-    }
 }
--- functional-programming/src/main/java/imperative/_Function.java
+++ functional-programming/src/main/java/imperative/_Function.java
@@ -1,12 +1,9 @@
 package imperative;
 
-import java.util.function.Function;
 
 public class _Function {
     public static void main(String[] args) {
-        Integer number = incrementByFunction.apply(1);
-        System.out.println(number);
+
     }
 
-    private static final Function<Integer, Integer> incrementByFunction = number -> number + 1;
 }
