cda259c3f2e6e7b623716cd9c74b255160b413bd
talibnabi <talibnabiyev@gmail.com>
2022-08-21 04:00:19 -0700

    commit

--- functional-programming/src/main/java/functional/App.java
+++ functional-programming/src/main/java/functional/App.java
@@ -2,6 +2,7 @@ package functional;
 
 public class App {
     //https://belief-driven-design.com/functional-programming-with-java-streams-190eda591a5/
+
     public static void main(String[] args) {
 
     }
