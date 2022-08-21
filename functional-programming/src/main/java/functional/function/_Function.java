8d992d0d1487112c28d9d03eaeefad94991efa48
talibnabi <talibnabiyev@gmail.com>
2022-08-21 05:49:52 -0700

    commit

--- functional-programming/src/main/java/functional/function/_Function.java
+++ functional-programming/src/main/java/functional/function/_Function.java
@@ -6,4 +6,5 @@ public class _Function {
     protected static final Function<Integer, Integer> incrementNumber = number -> number + 1;
     protected static final Function<Integer, String> incrementNumber2 = number -> String.valueOf(number + 1);
     protected static final Function<Integer, Integer> multiplyNumber = number -> number * 10;
+    
 }
