0f56ac11fe2c77ca09f69dd4bf2afa0f5faa3c0c
talibnabi <talibnabiyev@gmail.com>
2022-08-21 06:14:50 -0700

    commit

--- functional-programming/src/main/java/functional/consumer/App.java
+++ functional-programming/src/main/java/functional/consumer/App.java
@@ -2,6 +2,7 @@ package functional.consumer;
 
 public class App {
     public static void main(String[] args) {
-        _Consumer.customerConsumer.accept(new Customer("Alisa", 508312165));
+//        _Consumer.customerConsumer.accept(new Customer("Alisa", 508312165));
+        _Consumer.biCustomerConsumer.accept(new Customer("Alisa", 508312165), false);
     }
 }
--- functional-programming/src/main/java/functional/consumer/_Consumer.java
+++ functional-programming/src/main/java/functional/consumer/_Consumer.java
@@ -1,9 +1,11 @@
 package functional.consumer;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class _Consumer {
     protected static final Consumer<Customer> customerConsumer = customer ->
             System.out.println("Hello," + customer.getCustomerName() + ",thanks for registering with phone number: " + customer.getCustomerPhoneNumber());
-    
+    protected static final BiConsumer<Customer, Boolean> biCustomerConsumer = (customer, showPhoneNumber) ->
+            System.out.println("Hello," + customer.getCustomerName() + ",thanks for registering with phone number: " + (showPhoneNumber ? customer.getCustomerPhoneNumber() : "********"));
 }
