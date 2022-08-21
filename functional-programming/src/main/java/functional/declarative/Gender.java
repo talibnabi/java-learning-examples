d38a6909ef6d1b0d2b1282d599d7283faeb9a7f5
talibnabi <talibnabiyev@gmail.com>
2022-08-21 04:10:47 -0700

    commit

--- functional-programming/src/main/java/functional/declarative/Gender.java
+++ functional-programming/src/main/java/functional/declarative/Gender.java
@@ -1,4 +1,5 @@
 package functional.declarative;
 
 public enum Gender {
+    MALE, FEMALE
 }
--- functional-programming/src/main/java/functional/declarative/Person.java
+++ functional-programming/src/main/java/functional/declarative/Person.java
@@ -2,5 +2,26 @@ package functional.declarative;
 
 public class Person {
     private final String name;
+    private final Gender gender;
 
+    public Person(String name, Gender gender) {
+        this.name = name;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", gender=" + gender +
+                '}';
+    }
 }
