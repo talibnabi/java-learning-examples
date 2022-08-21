1542a6d5adaf33355e35bd66b64097d89c86c74b
talibnabi <talibnabiyev@gmail.com>
2022-08-21 05:15:18 -0700

    commit

--- functional-programming/src/main/java/functional/function/App.java
+++ functional-programming/src/main/java/functional/function/App.java
@@ -2,6 +2,12 @@ package functional.function;
 
 public class App {
     public static void main(String[] args) {
-        
+        Integer number = _Function.incrementNumber.apply(1);
+        String number2 = _Function.incrementNumber2.apply(3);
+        Integer multiplyNumber = _Function.multiplyNumber.apply(10);
+        Integer number3 = _Function.multiplyNumber.andThen(_Function.incrementNumber).apply(3);
+        Integer number4 = _Function.incrementNumber.compose(_Function.multiplyNumber).apply(3);
+        Integer number5 = _Function.incrementNumber.andThen(_Function.multiplyNumber).apply(4);
+        System.out.println(number5);
     }
 }
---
+++ functional-programming/src/main/java/functional/function/_Function.java
@@ -0,0 +1,9 @@
+package functional.function;
+
+import java.util.function.Function;
+
+public class _Function {
+    protected static final Function<Integer, Integer> incrementNumber = number -> number + 1;
+    protected static final Function<Integer, String> incrementNumber2 = number -> String.valueOf(number + 1);
+    protected static final Function<Integer, Integer> multiplyNumber = number -> number * 10;
+}
