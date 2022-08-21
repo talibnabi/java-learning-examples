a109390d825298f28db6d638eb63414c0bdfbcb7
talibnabi <talibnabiyev@gmail.com>
2022-08-21 05:56:16 -0700

    commit

--- functional-programming/src/main/java/functional/function/App.java
+++ functional-programming/src/main/java/functional/function/App.java
@@ -8,6 +8,7 @@ public class App {
         Integer number3 = _Function.multiplyNumber.andThen(_Function.incrementNumber).apply(3);
         Integer number4 = _Function.incrementNumber.compose(_Function.multiplyNumber).apply(3);
         Integer number5 = _Function.incrementNumber.andThen(_Function.multiplyNumber).apply(4);
-        System.out.println(number5);
+        Integer number6 = _Function.addAndMultiply.apply(number, multiplyNumber);
+        System.out.println(number6);
     }
 }
--- functional-programming/src/main/java/functional/function/_Function.java
+++ functional-programming/src/main/java/functional/function/_Function.java
@@ -1,10 +1,12 @@
 package functional.function;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class _Function {
     protected static final Function<Integer, Integer> incrementNumber = number -> number + 1;
     protected static final Function<Integer, String> incrementNumber2 = number -> String.valueOf(number + 1);
     protected static final Function<Integer, Integer> multiplyNumber = number -> number * 10;
-    
+    protected static final BiFunction<Integer, Integer, Integer> addAndMultiply = (numberAdd, numberMultiply) -> (numberAdd + 1) * (numberMultiply + 10);
+
 }
