291259e0bfb4946cec2fb0db1e3727af10b786f3
talibnabi <talibnabiyev@gmail.com>
2022-08-22 01:03:47 -0700

    commit

--- functional-programming/src/main/java/functional/predicate/App.java
+++ functional-programming/src/main/java/functional/predicate/App.java
@@ -1,11 +1,8 @@
 package functional.predicate;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class App {
     public static void main(String[] args) {
-
+        boolean check = _Predicate.isValidPhoneNumberStartAndLen.test("0708312165");
+        System.out.println(check);
     }
 }
--- functional-programming/src/main/java/functional/predicate/_Predicate.java
+++ functional-programming/src/main/java/functional/predicate/_Predicate.java
@@ -1,6 +1,11 @@
 package functional.predicate;
 
 
-public class _Predicate {
+import java.util.function.Predicate;
 
+public class _Predicate {
+    protected static final Predicate<String> isValidPhoneNumberStartAndLen =
+            phoneNumber -> phoneNumber.startsWith("070") && phoneNumber.length() == 8;
+    protected static final Predicate<String> isValidPhoneNumberContains =
+            phoneNumber -> phoneNumber.contains("831");
 }
