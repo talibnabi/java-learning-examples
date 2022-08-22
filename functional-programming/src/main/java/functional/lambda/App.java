d1a990ec1dda461a786fac45b37ff8fd40eeb430
talibnabi <talibnabiyev@gmail.com>
2022-08-22 07:19:53 -0700

    commit

--- functional-programming/src/main/java/functional/lambda/App.java
+++ functional-programming/src/main/java/functional/lambda/App.java
@@ -1,4 +1,7 @@
 package functional.lambda;
 
 public class App {
+    public static void main(String[] args) {
+        System.out.println(Lambdas.biFunctionLambda.apply("Alisa", 12));
+    }
 }
--- functional-programming/src/main/java/functional/lambda/Lambdas.java
+++ functional-programming/src/main/java/functional/lambda/Lambdas.java
@@ -1,5 +1,15 @@
 package functional.lambda;
 
-public class Lambdas {
+import java.util.Locale;
+import java.util.function.BiFunction;
 
+public class Lambdas {
+    protected static final BiFunction<String, Integer, String> biFunctionLambda =
+            (name, age) -> {
+                if (name.isBlank()) {
+                    throw new IllegalArgumentException("name is blank");
+                }
+                System.out.println(age);
+                return name.toUpperCase(Locale.ROOT);
+            };
 }
