218ee8a23b9521f2dffd027d9fae047207c26530
talibnabi <talibnabiyev@gmail.com>
2022-08-22 01:09:48 -0700

    commit

--- functional-programming/src/main/java/functional/predicate/App.java
+++ functional-programming/src/main/java/functional/predicate/App.java
@@ -6,6 +6,10 @@ public class App {
         boolean check2 = _Predicate.isValidPhoneNumberStartAndLen
                 .and(_Predicate.isValidPhoneNumberContains)
                 .test("0708312165");
-        System.out.println(check2);
+        boolean check3 = _Predicate.isValidPhoneNumberStartAndLen.negate().test("0708312165");
+        boolean check4 = _Predicate.isValidPhoneNumberStartAndLen
+                .or(_Predicate.isValidPhoneNumberContains)
+                .test("0708312165");
+        System.out.println(check4);
     }
 }
