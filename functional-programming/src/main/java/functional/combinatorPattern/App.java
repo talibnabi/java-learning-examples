67609c3d74b28058d7c056c918f5cc8bb2ee4c80
talibnabi <talibnabiyev@gmail.com>
2022-08-22 04:54:02 -0700

    commit

--- functional-programming/src/main/java/functional/combinatorPattern/App.java
+++ functional-programming/src/main/java/functional/combinatorPattern/App.java
@@ -1,7 +1,16 @@
 package functional.combinatorPattern;
 
+import java.time.LocalDate;
+
 public class App {
     public static void main(String[] args) {
-
+        CustomerValidation validation = new CustomerValidation();
+        boolean check = validation.isValid(new Customer(
+                "Talib",
+                "talibnabi@gmail.com",
+                "+994508312165",
+                LocalDate.of(2001, 12, 02)
+        ));
+        System.out.println(check);
     }
 }
--- functional-programming/src/main/java/functional/combinatorPattern/Customer.java
+++ functional-programming/src/main/java/functional/combinatorPattern/Customer.java
@@ -1,4 +1,43 @@
 package functional.combinatorPattern;
 
+import java.time.LocalDate;
+
 public class Customer {
+    private final String username;
+    private final String email;
+    private final String phoneNumber;
+    private final LocalDate DOB;
+
+    public Customer(String username, String email, String phoneNumber, LocalDate DOB) {
+        this.username = username;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.DOB = DOB;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public LocalDate getDOB() {
+        return DOB;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", DOB=" + DOB +
+                '}';
+    }
 }
