0404b522875ff04c058163e1fd6068b44f236b45
talibnabi <talibnabiyev@gmail.com>
2022-08-22 07:11:04 -0700

    commit

--- functional-programming/src/main/java/functional/finalSection/App.java
+++ functional-programming/src/main/java/functional/finalSection/App.java
@@ -1,7 +1,18 @@
 package functional.finalSection;
 
+import java.util.function.Consumer;
+
 public class App {
     public static void main(String[] args) {
-        
+        hello("Alisa", null, name -> System.out.println("lastname does not exists for " + name));
+    }
+
+    private static void hello(String firstName, String lastName, Consumer<String> callback) {
+        System.out.println(firstName);
+        if (lastName != null) {
+            System.out.println(lastName);
+        } else {
+            callback.accept(firstName);
+        }
     }
 }
