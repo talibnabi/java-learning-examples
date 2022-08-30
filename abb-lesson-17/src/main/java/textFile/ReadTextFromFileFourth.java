84881c1f9b9998fdf41ef48208310d2a64849292
talibnabi <talibnabiyev@gmail.com>
2022-08-30 05:12:35 -0700

    commit

---
+++ abb-lesson-17/src/main/java/textFile/ReadTextFromFileFourth.java
@@ -0,0 +1,26 @@
+package textFile;
+
+import java.io.*;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ReadTextFromFileFourth {
+    public static void main(String[] args) {
+        readTextFromFile();
+    }
+
+    protected static void readTextFromFile() {
+        String fileName = "students.txt";
+        File file = new File(fileName);
+        List<String> content;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            Stream<String> line = bufferedReader.lines();
+            content = line.collect(Collectors.toList());
+            content.forEach(System.out::println);
+        } catch (IOException ignored) {
+            System.out.println(fileName + " not found.");
+            System.exit(0);
+        }
+    }
+}
