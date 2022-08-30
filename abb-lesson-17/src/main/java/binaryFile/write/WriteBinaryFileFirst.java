d6d4db0ed74293d88dd5da9e156d16e3bfd776b4
talibnabi <talibnabiyev@gmail.com>
2022-08-30 05:50:52 -0700

    commit

--- abb-lesson-17/src/main/java/binaryFile/write/WriteBinaryFileFirst.java
+++ abb-lesson-17/src/main/java/binaryFile/write/WriteBinaryFileFirst.java
@@ -1,7 +1,22 @@
 package binaryFile.write;
 
+import java.io.File;
+import java.io.FileWriter;
+
 public class WriteBinaryFileFirst {
     public static void main(String[] args) {
+        writBinaryFile();
+    }
 
+    protected static void writBinaryFile() {
+        String fileName = "data.bin";
+        File file = new File(fileName);
+        try (FileWriter fileWriter = new FileWriter(file, true)) {
+            fileWriter.write(2);
+            fileWriter.write("Hi,dear");
+            fileWriter.write(4);
+        } catch (Exception ignored) {
+            System.out.println("It was ignored.");
+        }
     }
 }
