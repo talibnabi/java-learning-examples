e23c74e887ff71bcbbe5704a91716822faf1ad41
talibnabi <talibnabiyev@gmail.com>
2022-09-01 08:47:52 -0700

    commit

---
+++ abb-lesson-8/src/main/java/lesson/MemoryApp.java
@@ -0,0 +1,15 @@
+package lesson;
+
+class Cat {
+    String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    static String format = "My name is %s";
+}
+
+public class MemoryApp {
+
+}
