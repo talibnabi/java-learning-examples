fe9f0153e84e8e930160d7766ad8ee8d91014e9b
talibnabi <talibnabiyev@gmail.com>
2022-09-01 04:01:28 -0700

    commit

--- abb-lesson-5/src/main/java/lesson/Iteration.java
+++ abb-lesson-5/src/main/java/lesson/Iteration.java
@@ -73,6 +73,92 @@ public class Iteration {
     public static void main(String[] args) {
         int[] xs1 = new int[]{1, 3, 5, 7};
         int[] xs2 = {1, 3, 5, 7};
+        int min = min(xs2);
+        System.out.println(min);
+    }
+
+    // O(n)
+    // to find the minimal element
+    public static int min(int[] xs) {
+        int min = xs[0];                       // exception
+        for (int i = 0; i < xs.length; i++) {
+            if (xs[i] < min) min = xs[i];
+        }
+        return min;
+    }
+
+    static class Point {
+        int x;
+        int y;
+    }
+
+    static class Person {
+        String name;
+        int age;
+    }
+
+    static class ItemCount {
+        String name;
+        int count;
+        double price;
+    }
+
+    static class Cart {
+        ItemCount[] items;
+        double total;
+    }
+
+    static class Result {
+        // fields (attributes, ...)
+        int min;
+        int max;
+        double avg;
+
+        public Result(int min, int max, double avg) {
+            this.min = min;
+            this.max = max;
+            this.avg = avg;
+        }
+
+        public String represent() {
+            return String.format("min: %d, max: %d, avg: %f", min, max, avg);
+        }
+
+        @Override
+        public String toString() {
+            return represent();
+        }
+    }
+
+    // overloading
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public static void main7(String[] args) {
+        add(1, 2);
+//    add(1,"Jim");
+        add(1, 2, 5);
+    }
+
+    // encapsulation == combining
+    // minimum, maximum, average values
+    public static Result min_max_avg(int[] xs) {
+        int min = xs[0];
+        int max = xs[0];
+        int count = 0;
+        int sum = 0;
+        for (int i = 0; i < xs.length; i++) {
+            if (xs[i] < min) min = xs[i];
+            if (xs[i] > max) max = xs[i];
+            count++;
+            sum += xs[i];
+        }
+        return new Result(min, max, (double) sum / count);
     }
 
 }
