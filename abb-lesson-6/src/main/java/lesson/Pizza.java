c1f852f5026ed9db77c09ce81fae23e435a065b7
talibnabi <talibnabiyev@gmail.com>
2022-09-01 04:06:53 -0700

    commit

--- abb-lesson-6/src/main/java/lesson/Pizza.java
+++ abb-lesson-6/src/main/java/lesson/Pizza.java
@@ -8,5 +8,29 @@ public class Pizza {
         this.name = n;
         this.size = s;
     }
-    
+
+    @Override
+    public boolean equals(Object given) {
+        if (given == null) return false;
+        if (this == given) return true; // p1.equals(p1)
+        if (!(given instanceof Pizza that)) return false; // p1.equals(Apple)
+
+        return this.size.equals(that.size) // Integer.equals
+                && this.name.equals(that.name); // String.equals
+    }
+
+    public void printMe() {
+        System.out.printf(
+                "Pizza name %s of size %d\n",
+                name, size);
+    }
+
+    public static void main(String[] args) {
+        Pizza p1 = new Pizza("Margarita", 30);
+        Pizza p2 = new Pizza("Quattroformaggi", 60);
+        p1.printMe();
+        p2.printMe();
+    }
+
+
 }
