9692483856b27d091fa6bb41f87e9fd89eef94b6
talibnabi <talibnabiyev@gmail.com>
2022-09-01 04:05:10 -0700

    commit

---
+++ abb-lesson-6/src/main/java/lesson/Pizza.java
@@ -0,0 +1,12 @@
+package lesson;
+
+public class Pizza {
+    public String name;
+    public Integer size;
+
+    public Pizza(String n, Integer s) {
+        this.name = n;
+        this.size = s;
+    }
+    
+}
