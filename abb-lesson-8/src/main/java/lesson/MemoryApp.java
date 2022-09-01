7d94bd4739e94288edb1b8c3b5ecf41d88ead995
talibnabi <talibnabiyev@gmail.com>
2022-09-01 08:51:22 -0700

    commit

--- abb-lesson-8/src/main/java/lesson/MemoryApp.java
+++ abb-lesson-8/src/main/java/lesson/MemoryApp.java
@@ -12,4 +12,26 @@ class Cat {
 
 public class MemoryApp {
 
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        String user1 = "Alex";
+        String user2 = "Jim";
+        String user3 = "Alex";
+        String user4 = new String("Alex");
+        String user5 = "Alex";
+        System.out.println(user4.equals(user3));
+        System.out.println(user3==user5);
+        int x = 5;
+        Integer y = 5;
+        Cat cat = new Cat("Jacky");
+        Cat cat2 = new Cat("Mickey");
+        String format = Cat.format;
+        String format1 = cat.format;
+        String format2 = cat2.format;
+        System.out.println(format1);
+    }
+
 }
