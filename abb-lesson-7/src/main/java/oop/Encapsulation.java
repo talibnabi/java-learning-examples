edc3bc641be4fb1ed4a386fc2772890817be8b71
talibnabi <talibnabiyev@gmail.com>
2022-09-01 04:44:58 -0700

    commit

--- abb-lesson-7/src/main/java/oop/Encapsulation.java
+++ abb-lesson-7/src/main/java/oop/Encapsulation.java
@@ -1,4 +1,68 @@
 package oop;
 
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Person {
+    private String name;
+    private Integer age;
+    private final List<String> skills;
+
+    public Person(String name, Integer age, String... skills) {
+        this.name = name;
+        this.age = age;
+        this.skills = Arrays.stream(skills).collect(Collectors.toList());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        if (age < 18) throw new IllegalStateException("wrong age is provided");
+        this.age = age;
+    }
+
+    public List<String> getSkills() {
+        return skills;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", skills=" + skills +
+                '}';
+    }
+}
+
 public class Encapsulation {
+    public static void whatever(Person p) {
+        List<String> skills = p.getSkills();
+        skills.clear();
+        skills.add("Scala");
+    }
+
+
+    public static void main(String[] args) {
+        Person p = new Person("alex", 33, "Python", "Java");
+        System.out.println(p);
+        whatever(p);
+        System.out.println(p);
+        p.setAge(44);
+        System.out.println(p);
+
+    }
+
 }
--- abb-lesson-7/src/main/java/oop/Inheritance.java
+++ abb-lesson-7/src/main/java/oop/Inheritance.java
@@ -1,5 +1,39 @@
 package oop;
 
+abstract class Car {
+    public abstract void drive();
+}
+
+class Ford extends Car {
+
+    @Override
+    public void drive() {
+        System.out.println("The FORD is driving (overridden)");
+    }
+
+    public void accelerate() {
+        System.out.println("The FORD is accelerating");
+    }
+}
+
 public class Inheritance {
+    public static void main(String[] args) {
+//    Car car = new Car();
+//    car.drive();
+
+        Ford ford1 = new Ford();
+        ford1.drive();
+        ford1.accelerate();
+
+        Car ford2 = new Ford();
+        ford2.drive();
+//    ford2.accelerate();
+
+        System.out.println(ford1 instanceof Car);  // true
+        System.out.println(ford1 instanceof Ford); // true
+        System.out.println(ford2 instanceof Car);  // true
+        System.out.println(ford2 instanceof Ford); // true
+
+    }
 
 }
