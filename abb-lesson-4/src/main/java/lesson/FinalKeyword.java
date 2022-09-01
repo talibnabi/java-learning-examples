207801840339e2677d6a4c9c37b2c6d48c9af0dc
talibnabi <talibnabiyev@gmail.com>
2022-09-01 01:38:30 -0700

    commit

---
+++ abb-lesson-4/src/main/java/lesson/FinalKeyword.java
@@ -0,0 +1,14 @@
+package lesson;
+
+public final class FinalKeyword {
+    public static void main(String[] args) {
+        int i = 5;
+        i = 6;
+
+        // just a constant,
+        // you can't change it
+        // guarantees that you never change this var
+        final int j = 10;
+
+    }
+}
