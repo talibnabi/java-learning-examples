9cab017c3750310016b71c8166c83a8be26512b4
talibnabi <talibnabiyev@gmail.com>
2022-09-01 03:02:21 -0700

    commit

--- abb-lesson-4/src/main/java/lesson/envelope/EnvelopeFourth.java
+++ abb-lesson-4/src/main/java/lesson/envelope/EnvelopeFourth.java
@@ -1,4 +1,5 @@
 package lesson.envelope;
 
 public class EnvelopeFourth {
+    
 }
--- abb-lesson-4/src/main/java/lesson/envelope/EnvelopeThird.java
+++ abb-lesson-4/src/main/java/lesson/envelope/EnvelopeThird.java
@@ -1,4 +1,36 @@
 package lesson.envelope;
 
 public class EnvelopeThird {
+    public static void main(String[] args) {
+        final int width = 8;
+        final int height = 22;
+        String result = showEnvelope(22, 8);
+        System.out.println(result);
+    }
+
+    private static String showEnvelope(int width, int height) {
+        double k = (double) width / height;
+
+        final int firstRow = 0;
+        final int firstCol = 0;
+        final int lastRow = height - 1;
+        final int lastCol = width - 1;
+
+        StringBuilder result = new StringBuilder();
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                if (row == firstRow ||                   // firstRow
+                        col == firstCol ||                   // firstCol
+                        col == lastCol ||                    // lastCol
+                        row == lastRow ||                    // lastRow
+                        col == Math.round(row * k) ||        // diagonal 1
+                        col == lastCol - Math.round(row * k) // diagonal 2
+                )
+                    result.append("*");
+                else result.append(" ");
+            }
+            result.append("\n");
+        }
+        return result.toString();
+    }
 }
